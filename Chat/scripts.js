50a256536bdc7c7fd82dea09ec7b7f8f3d25e12a
pivs96 <jpalaznik@gmail.com>
2015-03-15 16:59:51 +0300

    Add local storiging

--- Chat/scripts.js
+++ Chat/scripts.js
@@ -1,45 +1,64 @@
-var username = "";
-var selectedRow = null;
-var isEditing = false;
-var connected = true;
-
-function run() {
-    var button = document.getElementById('add-button');
-	button.addEventListener('click', onAddButtonClick);
+(
+function() {
+    var username;
+    var selectedRow = null;
+    var isEditing = false;
+    var connected = true;
     
+    window.addEventListener('DOMContentLoaded', run);
+    window.addEventListener('resize', onResizeDocument);
+
+    var uniqueId = function() {
+        var date = Date.now();
+        var random = Math.random() * Math.random();
+    
+	   return Math.floor(date * random).toString();
+    };
+
+var newMessage = function(input, time) {
+	return {
+        name: username,
+        text: input,
+        time: time,
+        isMyMessage: true,
+		edited: false,
+		deleted: false,
+		id: uniqueId()
+	};
+};
+
+var messageList = [];
+    
+function run(e) {
+    username = restoreUsername() || "";
     var nameInput = document.getElementById('input-name');
     nameInput.addEventListener('focusout', onNameInput);
+    nameInput.value = username;
     
-    var input = document.getElementsByClassName('messageText')[0];
-	input.addEventListener('keydown', onTextInput);
+    document.getElementById('add-button').addEventListener('click', onAddButtonClick);
+    document.getElementsByClassName('messageText')[0].addEventListener('keydown', onTextInput);
+    document.getElementById('conection-item').addEventListener('click', onConnectionPress);
+    document.getElementsByClassName('icon')[0].addEventListener('click', onEditClick);
+    document.getElementsByClassName('icon')[1].addEventListener('click', onRemoveClick);
     
-    var edit = document.getElementsByClassName('icon')[0];
-    edit.addEventListener('click', onEditClick);
-    
-    var remove = document.getElementsByClassName('icon')[1];
-    remove.addEventListener('click', onRemoveClick);
-    
-    var conection = document.getElementById('conection-item');
-    conection.addEventListener('click', onConnectionPress);
-    
+    updateCounter();
+    onResizeDocument();
+    makeIconsUnvisible();
     var table = document.getElementsByClassName('table')[0];
     table.scrollTop = table.scrollHeight;
     
-    makeIconsUnvisible();
-    updateCounter();
-    onResizeDocument();
-    
-    $('#name').tooltip();
     $('#icon-edit').tooltip();
     $('#icon-remove').tooltip();
+    $('#name').tooltip();
     $('#messages-number').tooltip();
-    $('#message-input').popover({
-        delay: { "show": 500, "hide": 100 }
-    });
+    $('#message-input').popover({ delay: { "show": 1500 } });
     $('#input-name').popover();
+    
+    messageList = restoreMessages() || [];
+    updateMessages();
 }
 
-function onResizeDocument() {
+function onResizeDocument(e) {
     var all = document.getElementsByTagName('html')[0].clientHeight;
     var navbar = document.getElementsByClassName('navbar')[0].clientHeight;
     var input = document.getElementById('message-input').clientHeight;
@@ -48,7 +67,7 @@ function onResizeDocument() {
     document.getElementsByClassName('table')[0].style.height = height;
 }
 
-function onAddButtonClick() {
+function onAddButtonClick(e) {
     var message = document.getElementsByClassName('messageText')[0];
     
     if (isEditing == true) {  
@@ -56,13 +75,24 @@ function onAddButtonClick() {
         var bottomScroll = isScrollBottom(table);
         
         selectedRow.getElementsByClassName('list-group-item-text')[0].innerText = message.value;
-        message.value = '';
+        var messageId = selectedRow.getAttribute('id');
+            for (var i = 0; i < messageList.length; i++)
+                if (messageId == messageList[i].id) {
+                    messageList[i].message = message.value;
+                    if(!messageList[i].edited) {
+                        messageList[i].edited = true;
+                        selectedRow.childNodes[0].innerHTML += "<br>" + '<i class="glyphicon glyphicon-pencil"></i>';
+                    }
+                    break;
+                }
         
+        message.value = '';
         if(bottomScroll)
             table.scrollTop = table.scrollHeight;
         
         isEditing = false;
         selectedRow = null;
+        storeMessages();
         return;
     }
     
@@ -71,29 +101,32 @@ function onAddButtonClick() {
         nameInput = document.getElementById('input-name').focus();
         return;
     }
-
     
     if(!/\S/.test(message.value)) {
         message.value = '';
         return;
     }
     
-	addMessage(message.value);
+    var createdMessage = newMessage(message.value, getCurrentTime());
+	addMessage(createdMessage);
+    messageList.push(createdMessage);
     message.value = '';
+    storeMessages();
 	updateCounter();
 }
 
 function onNameInput(e) {
     var name = document.getElementById('input-name');
-    
     if(!/\S/.test(name.value)) {
         name.value = '';
         username = '';
+        storeUsername();
         $('#input-name').popover('show');
         return;
     }
     
     username = name.value;
+    storeUsername();
     $('#input-name').popover('hide');
 }
 
@@ -152,16 +185,12 @@ function setCaretPosition(textarea, pos) {
 	}
 }
 
-function addMessage(value) {
-	if(!value) {
-		return;
-	}
-          
+function addMessage(message) {          
 	var table = document.getElementsByClassName('table')[0];
     var bottomScroll = isScrollBottom(table);
     
     var row = table.insertRow(-1);
-    createRowValues(row, value);
+    createRowValues(row, message);
     
     if(bottomScroll)
         table.scrollTop = table.scrollHeight;
@@ -182,38 +211,43 @@ function isScrollBottom(table) {
     
     return bottomScroll;
 }
-
-function createRowValues(row, text) {
+    
+function createRowValues(row, message) {
 	row.classList.add('item');
-    row.classList.add('my-message');
-    row.id = "message" + document.getElementsByClassName("items")[0].rows.length;
+    if(message.isMyMessage)
+        row.classList.add('my-message');    
     row.addEventListener('click', onMessageClick);
+    row.setAttribute('id', message.id);
     
     var tdTime = document.createElement('td');
-	tdTime.classList.add('col-time');
-    tdTime.innerHTML = getCurrentTime();
-	row.appendChild(tdTime);
-    
     var tdMessage = document.createElement('td');
-    tdMessage.classList.add('col-message');
-    
     var divMessage = document.createElement('div');
-    divMessage.classList.add('list-group-item');
-    
     var user = document.createElement('h4');
-    user.classList.add('list-group-item-heading');
-    user.innerHTML = username;
-    divMessage.appendChild(user);
-    
     var wrap = document.createElement('div');
+    var text = document.createElement('p');
+    
+	tdTime.classList.add('col-time');
+    tdMessage.classList.add('col-message');
+    divMessage.classList.add('list-group-item');
+    user.classList.add('list-group-item-heading');
     wrap.classList.add('wrap');
+    text.classList.add('list-group-item-text');
     
-    var message = document.createElement('p');
-    message.classList.add('list-group-item-text');
-    message.innerText = text;
-    wrap.appendChild(message);
+    user.innerText = message.name;
+    text.innerText = message.text;
+    tdTime.innerHTML = message.time;
+    
+    if(message.deleted) {
+        tdTime.innerHTML += "<br>" + '<i class="glyphicon glyphicon-trash"></i>';
+        row.classList.add('deleted-message');
+    } else if(message.edited) {
+        tdTime.innerHTML += "<br>" + '<i class="glyphicon glyphicon-pencil"></i>';
+    }
+    
+	row.appendChild(tdTime);
+    divMessage.appendChild(user);
+    wrap.appendChild(text);
     divMessage.appendChild(wrap);
-    
     tdMessage.appendChild(divMessage);
 	row.appendChild(tdMessage);
 }
@@ -296,7 +330,26 @@ function onRemoveClick() {
     if(selectedRow == null)
         return;
     
-    selectedRow.parentNode.removeChild(selectedRow);
+    selectedRow.classList.remove('my-message');
+    var messageId = selectedRow.getAttribute('id');
+    var time;
+    for(var i = 0; i < messageList.length; ++i) {
+        if(messageId == messageList[i].id) {
+            messageList[i].deleted = true;
+            messageList[i].text = "This message has been deleted.";
+            messageList[i].isMyMessage = false;
+            time = messageList[i].time;
+            storeMessages();
+            break;
+        }
+    }
+    
+    selectedRow.childNodes[1].childNodes[0].childNodes[1].innerText = 'This message has been deleted.';
+    selectedRow.childNodes[0].innerHTML = time + "<br>" + '<i class="glyphicon glyphicon-trash"></i>';
+    selectedRow.getElementsByClassName('list-group-item')[0].classList.remove('active');
+    selectedRow.classList.remove('info');
+    selectedRow.classList.add('deleted-message');
+    
     selectedRow = null;
     makeIconsUnvisible();
     updateCounter();
@@ -326,4 +379,47 @@ function onConnectionSeted() {
     conection.classList.add('label-success');
     conection.textContent = "Connected";
 }
-
+    
+    function storeUsername() {
+        if(typeof(Storage) == "undefined") {
+            alert('localStorage is not accessible');
+            return;
+	   }
+        
+        localStorage.setItem("Chat Username", JSON.stringify(username));
+    }
+    
+    function storeMessages() {
+        if(typeof(Storage) == "undefined") {
+            alert('localStorage is not accessible');
+            return;
+	   }
+        
+        localStorage.setItem("Chat messageList", JSON.stringify(messageList));
+    }
+    
+    function restoreUsername() {
+        if(typeof(Storage) == "undefined") {
+            alert('localStorage is not accessible');
+            return;
+        }
+        
+        var name = localStorage.getItem("Chat Username");
+        return name && JSON.parse(name);
+    }
+    
+    function restoreMessages() {
+        if(typeof(Storage) == "undefined") {
+            alert('localStorage is not accessible');
+            return;
+        }
+        
+        var item = localStorage.getItem("Chat messageList");
+        return item && JSON.parse(item);
+    }
+    
+    function updateMessages() {
+        for (var i = 0; i < messageList.length; i++)
+            addMessage(messageList[i]);
+    }
+}())
